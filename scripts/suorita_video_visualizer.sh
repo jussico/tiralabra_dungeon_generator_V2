d8f207c08ad40ce4adb054ab8a444d33111b3d0d
Jussi Asp <jussi.asp@helsinki.fi>
2022-12-12 09:38:53 +0200

    big video script.

--- scripts/suorita_video_visualizer.sh
+++ scripts/suorita_video_visualizer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # python src/main.py 20 20 video
-python src/main.py 160 128 video
+# python src/main.py 160 128 video
 # python src/main.py 320 256 video
+python src/main.py 900 500 video
 
