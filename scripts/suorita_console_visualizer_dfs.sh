77faa753ce39591803ae70d66bf69260b722aba6
Jussi Asp <jussi.asp@helsinki.fi>
2023-01-02 02:47:07 +0200

    dev scripts updated.

--- scripts/suorita_console_visualizer_dfs.sh
+++ scripts/suorita_console_visualizer_dfs.sh
@@ -3,5 +3,10 @@
 # python src/main.py 5 5 console 666 dfs
 # python src/main.py 10 10 console 666 dfs dfs
 # python src/main.py 20 20 console 666 dfs
-python src/main.py 25 25 console 666 dfs dfs silent
+# python src/main.py 25 25 console 666 dfs dfs silent
+
+# dfs test
+# python src/main.py 32 16 console 666 dfs dfs silent 10
+python src/main.py 32 16 dummy 666 dfs dfs silent 10
+# python src/main.py 6 4 dummy 666 dfs dfs silent 10
 
